b4f56c3d7d0237e76f765b1729c955adc116a560
Tittaya Worawongtad <tittaya10407@gmail.com>
2022-05-13 22:38:18 +0700

    Add initialize method(style, effect and event when mouseClick)

--- src/gui/ControlPane.java
+++ src/gui/ControlPane.java
@@ -3,8 +3,10 @@ package gui;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import screen.MainMenu;
 import sharedObject.RenderableHolder;
 
 public class ControlPane extends VBox{
@@ -14,20 +16,11 @@ public class ControlPane extends VBox{
 	private Button exitButton;
 	
 	public ControlPane() {
-		startButton = new Button("Start");
-		instructionButton = new Button("Instruction");
-		creditButton = new Button("Credit");
-		exitButton = new Button("Exit");
 		
-		this.initializeButtonStyle(startButton);
-		this.initializeButtonStyle(instructionButton);
-		this.initializeButtonStyle(creditButton);
-		this.initializeButtonStyle(exitButton);
-		
-		//startButton.setOnMouseClicked(event -> Start() );
-		//instructionButton.setOnMouseClicked(event -> method() );
-		//creditButton.setOnMouseClicked(event -> method() );
-		//exitButton.setOnMouseClicked(event -> method() );
+		initializeStartButton();
+		initializeInstructionButton();
+		initializeCreditButton();
+		initializeExitButton();
 		
 		this.getChildren().addAll(startButton, instructionButton, creditButton, exitButton );
 		this.setAlignment(Pos.CENTER);
@@ -47,5 +40,54 @@ public class ControlPane extends VBox{
 		button.setOnMouseExited(event ->{
 			button.setEffect(null);
 		});
+	}
+	
+	public void initializeStartButton() {
+		startButton = new Button("Start");
+		this.initializeButtonStyle(startButton);
+		//startButton.setOnMouseClicked(event -> Start() );
+	}
+	
+	public void initializeInstructionButton() {
+		instructionButton = new Button("Instruction");
+		this.initializeButtonStyle(instructionButton);
+		instructionButton.setOnMouseClicked(event -> showInstructionWindow() );
+	}
+	
+	public void initializeCreditButton() {
+		creditButton = new Button("Credit");
+		this.initializeButtonStyle(creditButton);		
+		creditButton.setOnMouseClicked(event -> showCreditWindow() );
+	}
+	
+	public void initializeExitButton() {
+		exitButton = new Button("Exit");
+		this.initializeButtonStyle(exitButton);
+		exitButton.setOnMouseClicked(event -> System.exit(0) );
+	}
+	
+	
+	public void showCreditWindow() {
+		Pane showPane = MainMenu.getWindowPane();
+		Pane creditPane = MainMenu.getCreditPane();
+
+		if( showPane.getChildren().isEmpty() || !showPane.getChildren().get(0).equals(creditPane)) {
+			showPane.getChildren().clear();
+			showPane.getChildren().add(creditPane);
+		}else {
+			showPane.getChildren().clear();
+		}
+	}
+	
+	public void showInstructionWindow() {
+		Pane showPane = MainMenu.getWindowPane();
+		Pane instructionPane = MainMenu.getInstructionPane();
+
+		if( showPane.getChildren().isEmpty() || !showPane.getChildren().get(0).equals(instructionPane)) {
+			showPane.getChildren().clear();
+			showPane.getChildren().add(instructionPane);
+		}else {
+			showPane.getChildren().clear();
+		}
 	}
 }
