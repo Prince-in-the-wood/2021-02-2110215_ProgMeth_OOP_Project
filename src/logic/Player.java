392d784ad87cc391ddfea84bd2a8d47063b22c0e
Tittaya Worawongtad <tittaya10407@gmail.com>
2022-05-14 16:47:48 +0700

    Add getter/setter and constructor

--- src/logic/Player.java
+++ src/logic/Player.java
@@ -4,11 +4,30 @@ import entity.base.Emotion;
 import entity.base.Item;
 
 public class Player {
-	private Item itemInHand;
-	private Emotion playEmotion;
+	private static Item itemInHand;
+	private static Emotion playEmotion;
 	
 	public Player() {
-		
+		itemInHand = null;
+		playEmotion = Emotion.NORMAL;
 	}
 
+	public Item getItemInHand() {
+		return itemInHand;
+	}
+
+	public static Emotion getPlayEmotion() {
+		return playEmotion;
+	}
+
+	public static void setItemInHand(Item itemInHand) {
+		Player.itemInHand = itemInHand;
+	}
+
+	public static void setPlayEmotion(Emotion playEmotion) {
+		Player.playEmotion = playEmotion;
+	}
+	
+	
+	
 }
