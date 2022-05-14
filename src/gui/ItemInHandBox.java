f2bba49bd0930328b5be42adb0ed7a19329aa6c2
Tittaya Worawongtad <tittaya10407@gmail.com>
2022-05-14 21:45:35 +0700

    Add set/delete image method

--- src/gui/ItemInHandBox.java
+++ src/gui/ItemInHandBox.java
@@ -1,6 +1,8 @@
 package gui;
 
+import javafx.geometry.Insets;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
@@ -10,17 +12,25 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
 public class ItemInHandBox extends Pane{
-	
-	private Image itemImage;
+
+	private static ImageView itemImage;
 	
 	public ItemInHandBox() {
 		
 		BorderStroke borderStroke = new BorderStroke(Color.web("#FFBD51"), BorderStrokeStyle.SOLID,
 				  									new CornerRadii(10), new BorderWidths(5));
-		
 		this.setBorder(new Border(borderStroke));
 		this.setMaxSize(200, 140);
 		this.setMinSize(200, 140);
+		this.setPadding(new Insets(10));
+	}
+	
+	public static void setImageInBox(Image image) {
+		itemImage.setImage(image);
+	}
+	
+	public static void deleteImageInBox() {
+		itemImage.setImage(null);
 	}
  
 
