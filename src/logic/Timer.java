a6d18eb92afe0d8d137f5af32674946accf2d30d
Tittaya Worawongtad <tittaya10407@gmail.com>
2022-05-14 16:50:41 +0700

    Complete Timer

--- src/logic/Timer.java
+++ src/logic/Timer.java
@@ -2,38 +2,53 @@ package logic;
 
 public class Timer {
 	private int minute;
-	private int second;
+	private int seconds;
 	private int ms;
 	
 	public Timer(int m,int s, int ms) {
 		setMinute(m);
-		setSecond(s);
+		setSeconds(s);
 		setMs(ms);
 	}
 
-	// getter and setter
-	public int getMinute() {
-		return minute;
+	
+	public void decrementTimer(int amount) { //Decrease the timer by specified amount (milliseconds)
+		
+		if(isTimerOver()) {return;}
+		
+		ms -= amount;
+
+		while(ms < 0) {
+			if(isTimerOver()) {ms = 0; return;}
+			ms+=100;
+			seconds -= 1;	
+			while(seconds < 0) {
+				seconds+=60;	
+				minute -=1;
+			}
+		}
+
+	}
+	
+	public boolean isTimerOver() {
+		return minute<=0 && seconds<=0 && ms<=0;
+	}
+	
+	public String toString() {
+		return String.format("%02d:%02d:%02d",minute, seconds, ms);
 	}
 
 	public void setMinute(int minute) {
 		this.minute = minute;
 	}
 
-	public int getSecond() {
-		return second;
+	public void setSeconds(int seconds) {
+		this.seconds = seconds;
 	}
-
-	public void setSecond(int second) {
-		this.second = second;
-	}
-
-	public int getMs() {
-		return ms;
-	}
-
+	
 	public void setMs(int ms) {
 		this.ms = ms;
 	}
+
 
 }
