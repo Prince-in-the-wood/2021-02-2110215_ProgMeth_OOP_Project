356311084226cbf3ee313fa8d441067696ce2cf7
Tittaya Worawongtad <tittaya10407@gmail.com>
2022-05-15 15:15:59 +0700

    Add all necessary method

--- src/entity/furniture/Statue.java
+++ src/entity/furniture/Statue.java
@@ -1,61 +1,90 @@
 package entity.furniture;
 
 import entity.base.Updateable;
+import javafx.scene.canvas.GraphicsContext;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import entity.base.Furniture;
 
 public class Statue extends Furniture implements Updateable{
+	 
+	private final ArrayList<Character> characterSet = new ArrayList<Character>(
+													  Arrays.asList('M', 'H' ,'I' , 'P' , 'E', 'N' ,'A' , 'S' ,'O' ));
+	private final double translationDis = 50;
 	
-	private String letterOnState;
-	private String answerLetter;
-	private boolean isMatch;
+	private char letterOnStatue;
+	private char answerLetter;
+	private boolean isUpdated;
 
-	public Statue(String name, int xPosition, int yPosition, String letterOnStatue, String answerLetter, boolean isMatch) {
+	public Statue(String name, int xPosition, int yPosition, char letterOnStatue, char answerLetter) {
 		super(name, xPosition, yPosition);
 	
-		this.letterOnState = letterOnStatue;
+		this.letterOnStatue = letterOnStatue;
 		this.answerLetter = answerLetter;
-		setMatch(false);
 	}
 
 
 	//setter and getter
-	public String getLetterOnState() {
-		return letterOnState;
+	public char getLetterOnStatue() {
+		return letterOnStatue;
 	}
 
-	public void setLetterOnState(String letterOnState) {
-		this.letterOnState = letterOnState;
+	public void setLetterOnStatue(char letterOnStatue) {
+		this.letterOnStatue = letterOnStatue;
 	}
 
 
-	public String getAnswerLetter() {
+	public char getAnswerLetter() {
 		return answerLetter;
 	}
 
-	public void setAnswerLetter(String answerLetter) {
+	public void setAnswerLetter(char answerLetter) {
 		this.answerLetter = answerLetter;
 	}
 
 	public boolean isMatch() {
-		return isMatch;
+		return this.answerLetter == this.letterOnStatue;
 	}
 
-	public void setMatch(boolean isMatch) {
-		this.isMatch = isMatch;
+	public ArrayList<Character> getCharacterSet() {
+		return characterSet;
 	}
 
 
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
-		
+		isUpdated = true;
 	}
 
 
 	@Override
 	public boolean isUpdated() {
-		// TODO Auto-generated method stub
-		return false;
+		return isUpdated;
 	}
 	
+	@Override
+	public void observe() {
+		
+		if( !isUpdated() ) {
+			super.observe();			
+		}else {
+			
+			int i = ( characterSet.indexOf(this.letterOnStatue) + 1 ) % characterSet.size();
+			this.setLetterOnStatue( characterSet.get(i));
+		
+		}
+	}
+	
+	@Override
+	public void draw(GraphicsContext gc) {
+		
+		super.draw(gc);
+		
+		double x = super.getxPosition();
+		double y = super.getyPosition();
+		
+		gc.fillText(Character.toString(letterOnStatue), x, y + translationDis );
+		
+	}
 }
