51e33f438d4eb25898b257ead83561d1bd68b99f
Tittaya Worawongtad <tittaya10407@gmail.com>
2022-05-15 13:35:01 +0700

    Add all necessary method

--- src/entity/furniture/Sofa.java
+++ src/entity/furniture/Sofa.java
@@ -6,18 +6,26 @@ import entity.base.Cuttable;
 import entity.base.Furniture;
 import entity.base.Container;
 import entity.base.Item;
+import entity.item.Key;
+import entity.item.Note;
+import entity.item.PocketKnife;
+import gui.DialoguePane;
+import javafx.scene.canvas.GraphicsContext;
+import logic.Player;
+import sharedObject.RenderableHolder;
 
 public class Sofa extends Furniture implements Cuttable{
 
 	private boolean isCut;
 	private boolean isSomethingBehind;
 	private ArrayList<Item> thingBehind;
+	private String imageString;
 	
 	public Sofa(String name, int xPosition, int yPosition, boolean isSomethingBehind) {
-		super(name, xPosition, yPosition);
-		// TODO Auto-generated constructor stub
+		super(name, xPosition, yPosition);		
 		
-		setCut(false);	
+		setImageString(name);
+		setIsCut(false);	
 		setIsSomethingBehind(isSomethingBehind);
 	}
 
@@ -25,18 +33,72 @@ public class Sofa extends Furniture implements Cuttable{
 
 	@Override
 	public void cut() {
-		// TODO Auto-generated method stub
 		
+		this.setIsCut(true);
+		this.setImageString("CutSofa");
+		
+		for( int i = thingBehind.size() - 1 ; i >= 0 ; i++ ) {
+			thingBehind.get(i).setIsVisible(true);
+		}
 	}
 
 	@Override
 	public boolean isCut() {
-		// TODO Auto-generated method stub
 		return isCut;
 	}
+	
+	@Override
+	public void useItem() {
+		if( !isSomethingBehind || isCut() )
+			super.useItem();
+		else {
+			Item itemInHand = Player.getItemInHand();
+			if( itemInHand instanceof PocketKnife && ((PocketKnife)itemInHand).useWith(this) ) {
+				this.cut();
+			}else {
+				DialoguePane.setGameText("I think it doesn't fit here.");
+			}
+		}
+	}
+	
+	@Override
+	public void observe() {
+		if( !isSomethingBehind() ) {
+		
+			super.observe();
+			
+		}else if( !isCut() ) {
+			
+			DialoguePane.setGameText("I think there is something under the sofa");			
+		
+		}else if( thingBehind.size() > 0 ) {
+			
+			for( int i = thingBehind.size() - 1 ; i >= 0 ; i++ ) {
+				
+				if( thingBehind.get(i) instanceof Note ) {
+					((Note)thingBehind.get(i)).read();
+				}
+				
+				if( thingBehind.get(i) instanceof PocketKnife ) {
+					((PocketKnife)thingBehind.get(i)).pick();
+					thingBehind.remove(i);
+				}
+				
+				if( thingBehind.get(i) instanceof Key ) {
+					((Key)thingBehind.get(i)).pick();
+					thingBehind.remove(i);
+				}
+			}
+		}else {
+			
+			DialoguePane.setGameText("Little sorry, to make it's torn");			
+		
+		}
 
+	}
+	
 
-	public void setCut(boolean isCut) {
+	public void setIsCut(boolean isCut) {
 		this.isCut = isCut;
 	}
 
@@ -58,10 +120,25 @@ public class Sofa extends Furniture implements Cuttable{
 		return thingBehind;
 	}
 
-
-
 	public void setThingBehind(ArrayList<Item> thingBehind) {
 		this.thingBehind = thingBehind;
+	}
+
+
+
+	public String getImageString() {
+		return imageString;
+	}
+
+
+
+	public void setImageString(String imageString) {
+		this.imageString = imageString;
+	}
+	
+	@Override
+	public void draw(GraphicsContext gc) {
+		gc.drawImage(RenderableHolder.furnitureSprite.get(this.imageString) , super.getxPosition(), super.getyPosition());
 	}
 	
 }
