a84345a0b481653bd132c570e2696d3a27ebfc2e
Tittaya Worawongtad <tittaya10407@gmail.com>
2022-05-15 15:50:35 +0700

    Add all necessary method

--- src/entity/furniture/TableWithNote.java
+++ src/entity/furniture/TableWithNote.java
@@ -1,11 +1,44 @@
 package entity.furniture;
 
+import java.util.ArrayList;
+
 import entity.base.Container;
+import entity.base.Item;
+import entity.item.Key;
+import entity.item.Note;
+import entity.item.PocketKnife;
 
 public class TableWithNote extends Container{
 
 	public TableWithNote(String name, int xPosition, int yPosition) {
 		super(name, xPosition, yPosition);
+	}
+	
+	@Override
+	public void observe() {
+		
+		ArrayList<Item> item = this.getItem();
+		
+		if(item.size() >= 0 ) {
+			
+			for( int i = item.size() - 1 ; i >= 0 ; i++ ) {
+				if( item.get(i) instanceof Note ) {
+					((Note)item.get(i)).read();
+				}
+				
+				if( item.get(i) instanceof PocketKnife ) {
+					((PocketKnife)item.get(i)).pick();
+					item.remove(i);
+				}
+				
+				if( item.get(i) instanceof Key ) {
+					((Key)item.get(i)).pick();
+					item.remove(i);
+				}
+			}
+		}else{
+			super.observe();
+		}
 	}
 
 }
--- src/entity/furniture/Window.java
+++ src/entity/furniture/Window.java
@@ -3,41 +3,66 @@ package entity.furniture;
 import entity.base.Furniture;
 import entity.base.Lockable;
 import entity.base.Openable;
+import gui.DialoguePane;
+import javafx.scene.canvas.GraphicsContext;
+import sharedObject.RenderableHolder;
 
 public class Window extends Furniture implements Openable{
 
 	private boolean isOpened;
+	private String imageString;
 
 	public Window(String name, int xPosition, int yPosition) {
 		super(name, xPosition, yPosition);
-		// TODO Auto-generated constructor stub
-		setOpened(false);
+		setIsOpened(false);
+		setImageString(name);
 	}
 
 	@Override
 	public void open() {
-		// TODO Auto-generated method stub
-		
+		setImageString("OpenedWindow");
+		setIsOpened(true);
 	}
 
 	@Override
 	public boolean isOpened() {
-		// TODO Auto-generated method stub
 		return isOpened;
 	}
 
 
 	@Override
 	public void close() {
-		// TODO Auto-generated method stub
-		
+		setImageString(this.getName());
+		setIsOpened(false);
 	}
 
-	
+	@Override
+	public void observe() {
+		if( this.isOpened() )
+			this.close();
+		else
+			this.isOpened();
+		
+		DialoguePane.setGameText("Why everything is silent?");
+	}
 	// setter and getter
 
-	public void setOpened(boolean isOpened) {
+	public void setIsOpened(boolean isOpened) {
 		this.isOpened = isOpened;
 	}
+
+	public String getImageString() {
+		return imageString;
+	}
+
+	public void setImageString(String imageString) {
+		this.imageString = imageString;
+	}
+	
+	@Override
+	public void draw(GraphicsContext gc) {
+		gc.drawImage(RenderableHolder.furnitureSprite.get(this.getImageString()), this.getxPosition(), this.getyPosition());
+	}
+	
 
 }
