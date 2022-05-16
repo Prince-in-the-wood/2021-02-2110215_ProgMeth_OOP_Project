4baada593450a2f00f1bcb0310269cab7c8c7875
Tittaya Worawongtad <tittaya10407@gmail.com>
2022-05-16 17:55:25 +0700

    get PlayerEmotion to use

--- src/gui/CharacterPane.java
+++ src/gui/CharacterPane.java
@@ -4,18 +4,19 @@ import entity.base.Emotion;
 import javafx.geometry.Pos;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import logic.Player;
 import sharedObject.RenderableHolder;
 
 public class CharacterPane extends StackPane {
 
 	public CharacterPane() {
-		ImageView img = new ImageView(RenderableHolder.characterBody.get(Emotion.NORMAL));
+		ImageView img = new ImageView(RenderableHolder.characterBody.get(Player.getPlayerEmotion()));
 		img.setFitHeight(350);
 		img.setPreserveRatio(true);
 		
 		this.setAlignment(Pos.CENTER);
 		this.getChildren().add(img);
-		
+	
 		//this.setPadding(new Insets(20));
 	}
 
